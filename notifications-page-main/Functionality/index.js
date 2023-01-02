2ee8e64789e53dcfbec7e478d5c3da26ca53e582
LouisBenedict <louisloh100@gmail.com>
2023-01-03 00:47:54 +0800

    fix

--- notifications-page-main/Functionality/index.js
+++ notifications-page-main/Functionality/index.js
@@ -1,4 +1,20 @@
+const notification = document.querySelectorAll('.notification');
+const numbers = document.getElementById('number');
+const markRead = document.getElementById('mark-read');
 
+numbers.innerText = notification.length;
+
+const mark = () => {
+    const notification = document.querySelectorAll('.notification');
+    const numbers = document.getElementById('number');
+    const markRead = document.getElementById('mark-read');
+
+
+    console.log('Marked as read');
+    numbers.innerText = 0;
+}
+
+markRead.addEventListener('click', mark);
 
 // const markRead = () => {
 //     const markRead = document.getElementById('mark-read');
