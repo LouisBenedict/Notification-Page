ea561b391e8819e4081ec1b03cb9d7407c731e2f
LouisBenedict <louisloh100@gmail.com>
2023-01-03 00:15:44 +0800

    initial commit

---
+++ notifications-page-main/Functionality/index.js
@@ -0,0 +1,13 @@
+
+
+// const markRead = () => {
+//     const markRead = document.getElementById('mark-read');
+
+//     markRead.addEventListener('click', () => {
+//         const notifications = document.getElementsByClassName('notification');
+
+//         for (let i = 0; i < notifications.length; i++) {
+//             notifications[i].classList.add('read');
+//         }
+//     });
+// }
